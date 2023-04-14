e2ad9a6e4e5410f6b473a3f95c806c60f1f2fc34
AndreySozinov <savrey@ya.ru>
2023-04-14 20:02:46 +0500

    Done

--- Matrix.py
+++ Matrix.py
@@ -36,7 +36,12 @@ class Matrix:
     # def __mul__(self, other):
     #     if len(self.data[0]) != len(other.data):
     #         return -1
-    #
+    #     result = [[0] * len(other.data[0]) for i in range(len(self.data))]
+    #     for i in range(len(self.data)):
+    #         k = 0
+    #         for j in range(other.data):
+    #             k += self.data[i][j] * other.data[j][i]
+    #         result[i][]
 
 
 if __name__ == '__main__':
